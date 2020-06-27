56b84939f9b6798d41710c76325f948b23f1f0ef
raniel <rbhrbh65@gmail.com>
2020-06-27 10:40:42 +0300

    native

--- js/apps/mister-email/email-cmps/email-header.cmp.js
+++ js/apps/mister-email/email-cmps/email-header.cmp.js
@@ -5,8 +5,7 @@ import emailFilter from '../email-cmps/email-filter.cmp.js'
 
 export default {
     template:`
-    <section class="email-header">
-        
+    <section class="email-header">       
         <email-filter></email-filter>  
     </section>
     
--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -2,14 +2,27 @@ export default {
     template: `
     <header class="app-header flex align-center space-between">
       <h1>app sus</h1>
-      <nav class=" flex space-between">            
+      <nav class=" flex space-between"> 
+      <component v-if="currInputComponent" :is="currInputComponent" ></component>        
        <router-link to="/about"> About </router-link> 
        <router-link to="/"> Home Page </router-link> 
        <router-link to="/book"> book App </router-link> 
        <router-link to="/keep"> Miss keep </router-link> 
-       <router-link to="/email"> mister email </router-link> 
+       <router-link @click.native="consol" to="/email"> mister email </router-link> 
       </nav>
     </header>
-    `
+    `,
+    data(){
+      return {
+        currInputComponent: null
+      }
+    },
+    methods:{
+
+      consol(){
+        console.log('clicked')
+      }
+    }
+    
 
 }

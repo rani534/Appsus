c2f83fb4c69ba88ca47f4bea91b432b67b3c67a2
raniel <rbhrbh65@gmail.com>
2020-06-28 12:52:22 +0300

    ww

--- js/apps/mister-email/email-cmps/add-email.cpm.js
+++ js/apps/mister-email/email-cmps/add-email.cpm.js
@@ -9,7 +9,7 @@ export default {
             <textarea v-model="newEmail.body" class="email-body-textarea" placeholder=""></textarea>
             <div class="add-btns-container flex space-between">
                 <button class="btn send-btn">Send</button>
-                <span @click="backToList" class="delete-btn"><i class="fas fa-trash-alt"></i></span> 
+                <span @click="deleted" class="delete-btn"><i class="fas fa-trash-alt"></i></span> 
             </div>
         </form>
     </section>
@@ -24,11 +24,11 @@ export default {
     },
     methods: {
         send() {
-            this.$emit('sended')
+            this.$emit('backToList')
             emailService.addEmail(this.newEmail)
         },
-        backToList(){
-            this.$emit('sended')
+        deleted(){
+            this.$emit('backToList')
         }
     },
     mounted() {
